89f02cedeab7d91838133398285dc758961d5c49
msndogui@hotmail.com.br <msndogui@hotmail.com.br>
2014-02-25 11:14:59 -0300

    Criando a Listagem de Pacientes

--- src/br/com/senacrs/clinica/actions/PacientesActions.java
+++ src/br/com/senacrs/clinica/actions/PacientesActions.java
@@ -1,9 +1,12 @@
 package br.com.senacrs.clinica.actions;
 
+import java.util.List;
 import java.util.Scanner;
 
+import br.com.senacrs.clinica.dao.PacientesDao;
 import br.com.senacrs.clinica.library.ActionMenu;
 import br.com.senacrs.clinica.library.ActionController;
+import br.com.senacrs.clinica.models.Paciente;
 
 public class PacientesActions extends ActionController implements ActionMenu {
 
@@ -14,7 +17,44 @@ public class PacientesActions extends ActionController implements ActionMenu {
 
 	@Override
 	public void menu() {
-		
-		
+		out.println();
+		out.println("[1] Cadastrar");
+		out.println("[2] Listar");
+		out.println("[0] Sair");
+
+		switch(sc.nextInt()){
+		case 1: cadastraPaciente(); break;
+		case 2: mostraPacienteLista(); break;	
+		case 0: return;
+		default: out.println("Opcao invalida"); break;
+		}
+		menu();
+	}
+
+	private void mostraPacienteLista(){
+		out.println("-- Lista de Pacientes --");
+		out.println();
+		List<Paciente> pacientes = PacientesDao.getPacientesList();
+
+		if(pacientes.size() > 0){
+			for(Paciente paciente : pacientes){
+				out.println("ID: " + paciente.getId() +
+							" Nome: " + paciente.getNome()+
+							" CPF: " + paciente.getCpf()+
+							" Telefone: " + paciente.getTelefone()
+						);
+			}
+		} else {
+			out.println("Sem pacientes cadastrados.");
+		}
+		out.println();
+		pause();
+
+
+	}
+
+	private void cadastraPaciente(){
+
+
 	}
 }
